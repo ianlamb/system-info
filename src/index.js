67c64fcfa3b0cb7385eefed432f61d8a253135e4
Ian Lamb <ianlamb32@gmail.com>
2020-05-18 21:20:10 -0700

    error boundary stuff

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import ErrorBoundary from 'react-error-boundary'
+
 // import './vendor/modernizr'
+import './shims/webrtc-adapter'
 import './polyfills/String.prototype.padStart'
+
 import './index.css'
 import './svg-icons.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const FallbackComponent = ({ componentStack, error }) => (
+  <h1 className="App-error">It's borked, sorry :(</h1>
+)
+
+ReactDOM.render(
+  <ErrorBoundary FallbackComponent={FallbackComponent}>
+    <App />
+  </ErrorBoundary>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
